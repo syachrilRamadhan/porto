d525152fedb688aa275156d9c98ab413f84d48c4
syachrilRamadhan <syachrilbls99@gmail.com>
2024-08-16 03:55:14 +0700

    feat: (component) add service page

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -23,7 +23,7 @@ const stats = [
 
 const Stats = () => {
   return (
-    <section>
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div>
         <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
