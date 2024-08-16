414d99398870c7cd9b43cb104eb767581bda9490
syachrilRamadhan <syachrilbls99@gmail.com>
2024-08-17 01:13:29 +0700

    feat: add resume page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -35,7 +35,7 @@ const Service = () => {
               <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
                 <div className="w-full flex justify-between items-center">
                   <div className="text-5xl font-bold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
-                  <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:rotate-45">
+                  <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
                     <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
